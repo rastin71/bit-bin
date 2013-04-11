56d28e932a724bd5bc36bd1478d253faf00029cf
Ron Astin <ron.astin@susies-iphone.bluetech.local>
2013-04-11 09:51:55 -0700

    Screen Buffer class implemented, does proper corner detection which is a precursor to buffering vs drawing the whole world every render cycle.

--- Animations/src/GamePlayState.java
+++ Animations/src/GamePlayState.java
@@ -1,170 +1,64 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.MouseListener;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-//JRA: Original Code Begin
 public class GamePlayState extends BasicGameState implements MouseListener {
 
 	int stateID = 0;
 	public GamePlayState(int state) {stateID = state;}
 	public int getID() {return stateID;}
-	private Image cursor = null;
-	private ArrayList<Image> alTiles = null;
 	private ScreenBuffer sbVisable = null;
-	private static final int MAP_SIZE = 256;
-	private static final int SCREEN_WIDTH = 1024;
-	private static final int SCREEN_HEIGHT = 768;
-	private static final double SIN45 = Math.sin(Math.toRadians(45));
-	private static final double COS45 = Math.cos(Math.toRadians(45));
-	// private int tzero_org_x = 
-	private Image mark = null;
-	private int X_OFF = 0;
-	private int Y_OFF = 0;
-	int TL_X, TL_Y;
-	float FL_X, FL_Y;
+	private int x_off = 0;
+	private int y_off = 0;
 	
 	public void init(GameContainer container, StateBasedGame arg1) throws SlickException {
-		int i;
 		container.getInput().enableKeyRepeat();
-		sbVisable = new ScreenBuffer();
-		Image img0 = new Image("res/tile.png");
-		Image img1 = new Image("res/tile2.png");
-		Image imgx = new Image("res/tile_x.png");
-		Image imgy = new Image("res/tile_y.png");
-		cursor = new Image("res/mouse.png");
-		mark = new Image("res/tile3.png");
-		try {
-			InputStreamReader in = new InputStreamReader(new FileInputStream("res/mapdata.txt"), "US-ASCII");
-			alTiles = new ArrayList<Image>();
-			while ((i = in.read()) != -1) {
-				switch (i) {
-				case '0': 
-					alTiles.add(img0);
-					break;
-				case '1':
-					alTiles.add(img1);
-					break;
-				case 'x':
-					alTiles.add(imgx);
-					break;
-				case 'y':
-					alTiles.add(imgy);
-					break;
-				}
-			}
-			in.close();
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		sbVisable = new ScreenBuffer("res/mapdata.txt");
     }
 
-	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
-			throws SlickException {
-		int i, j, x, y;
-		// Shifting works because effective tile dimensions are all base 2 values.
-		for (i = 0; i < MAP_SIZE; i++) {
-			x = i << 4;
-			for (j = MAP_SIZE - 1; j >= 0; j--) {
-		    	y = j << 4;
-		        alTiles.get(i * MAP_SIZE + j).draw((x << 1) + (y << 1) + X_OFF, (x - y) + Y_OFF);
-		    }
-		}
-		cursor.draw(512.0f, 384.0f);
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		
-		i = 20;
-		g.drawString("Y Off: " + Y_OFF, 10, i += 15);
-		g.drawString("X Off: " + X_OFF, 10, i += 15);
-//		g.drawString("(0,0) YOff: " + (((-X_OFF - 2*-Y_OFF) / Math.sqrt(16*16+32*32))), 10, i += 15);
-//		g.drawString("(0,0) XOff: " + (((-X_OFF + 2*-Y_OFF) / Math.sqrt(16*16+32*32))), 10, i += 15);
-		g.drawString("(0,0) YOff: " + (((-X_OFF - 2*-Y_OFF)/68.0)), 10, i += 15);
-		g.drawString("(0,0) XOff: " + (((-X_OFF + 2*-Y_OFF)/68.0)), 10, i += 15);
-		
-		g.drawString("TL Pixel: (" + -X_OFF + "," + -Y_OFF + ")", 10, i += 25);
-		g.drawString("TR Pixel: (" + (-X_OFF + SCREEN_WIDTH) + "," + -Y_OFF + ")", 10, i += 15);
-		g.drawString("BL Pixel: (" + -X_OFF + "," + (-Y_OFF + SCREEN_HEIGHT) + ")", 10, i += 15);
-		g.drawString("BR Pixel: (" + (-X_OFF + SCREEN_WIDTH) + "," + (-Y_OFF + SCREEN_HEIGHT) + ")", 10, i += 15);
-		g.drawString("Mid Pixel: (" + (-X_OFF + SCREEN_WIDTH/2) + "," + (-Y_OFF + SCREEN_HEIGHT/2) + ")", 10, i += 15);
-
-		if (TL_X > -1 && TL_X < 256 && TL_Y > -1 && TL_Y < 256) {
-			mark.draw((TL_X << 5 + TL_Y << 5) + X_OFF, (TL_X << 4) - (TL_Y << 4) + Y_OFF);
-		}
-		
-		// alTiles.get(i * MAP_SIZE + j).draw((i * 32) + (j * 32) + X_OFF, (i * 16 - j * 16) + Y_OFF);
-		// Tile @ (0,0): 
-	
-		g.drawString("Mid Point : (" + -.5f * ((-Y_OFF - SCREEN_HEIGHT/2)/17.0f + (-X_OFF - SCREEN_WIDTH/2)/34.0f) + "," +
-				.5f * ((-Y_OFF - SCREEN_HEIGHT/2)/17.0f - (-X_OFF - SCREEN_WIDTH/2)/34.0f) + ")", 10, i += 25);
-
-		g.drawString("(0,0) Point : (" + (.5f * (-Y_OFF/17.0f + -X_OFF/34.0f) - .5f) + "," +
-				(-.5f * (-Y_OFF/17.0f - -X_OFF/34.0f)) + ")", 10, i += 15);
-
-		g.drawString("(1024,0) Point : (" + (.5f * ((-Y_OFF)/17.0f + (-X_OFF - SCREEN_WIDTH)/34.0f) + .5f) + "," +
-				(.5f * ((-Y_OFF)/17.0f - (-X_OFF - SCREEN_WIDTH)/34.0f) - .5f) + ")", 10, i += 15);
-
-		g.drawString("(0,768) Point : (" + (-.5f * ((Y_OFF - SCREEN_HEIGHT)/17.0f + (X_OFF)/34.0f) - .5f) + "," +
-				(.5f * ((-Y_OFF - SCREEN_HEIGHT)/17.0f - (-X_OFF)/34.0f) + .5f) + ")", 10, i += 15);
-
-		g.drawString("(1024,768) Point : (" + (-.5f * ((-Y_OFF - SCREEN_HEIGHT)/17.0f + (-X_OFF - SCREEN_WIDTH)/34.0f) + .5f) + "," +
-				(.5f * ((-Y_OFF - SCREEN_HEIGHT)/17.0f - (-X_OFF - SCREEN_WIDTH)/34.0f) + .5f) + ")", 10, i += 15);	
-		g.drawString("TL_X: " + TL_X, 10, i += 15);
-		g.drawString("TL_Y: " + TL_Y, 10, i += 15);
-		g.drawString("FL_X: " + FL_X, 10, i += 15);
-		g.drawString("FL_Y: " + FL_Y, 10, i += 15);
+		sbVisable.render(x_off, y_off);			
 	}
 
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 		throws SlickException {
+		
 		int step = 1;
 		if (gc.getInput().isKeyPressed(Input.KEY_Q)) {
 			gc.exit();
 		}
 		if (gc.getInput().isKeyPressed(Input.KEY_UP)) {
-			Y_OFF+=step;
+			y_off+=step;
 		}
 		if (gc.getInput().isKeyPressed(Input.KEY_DOWN)) {
-			Y_OFF-=step;
+			y_off-=step;
 		}
 		if (gc.getInput().isKeyPressed(Input.KEY_LEFT)) {
-			X_OFF+=step;
+			x_off+=step;
 		}
 		if (gc.getInput().isKeyPressed(Input.KEY_RIGHT)) {
-			X_OFF-=step;
+			x_off-=step;
 		}
 		if (gc.getInput().isKeyPressed(Input.KEY_I)) {
-			Y_OFF+=(10*step);
+			y_off+=(10*step);
 		}
 		if (gc.getInput().isKeyPressed(Input.KEY_K)) {
-			Y_OFF-=(10*step);
+			y_off-=(10*step);
 		}
 		if (gc.getInput().isKeyPressed(Input.KEY_J)) {
-			X_OFF+=(10*step);
+			x_off+=(10*step);
 		}
 		if (gc.getInput().isKeyPressed(Input.KEY_L)) {
-			X_OFF-=(10*step);
+			x_off-=(10*step);
 		}
-		FL_X = .5f * (-Y_OFF/17.0f + -X_OFF/34.0f);
-		FL_Y = .5f * (-Y_OFF/17.0f - -X_OFF/34.0f);
-		TL_X = Math.round(FL_X);
-		TL_Y = Math.round(FL_Y);
+		
+		sbVisable.update(x_off, y_off);
+
 	}
 
 	public void mouseClicked(int button, int x, int y, int clickCount) {
--- Animations/src/ScreenBuffer.java
+++ Animations/src/ScreenBuffer.java
@@ -1,20 +1,103 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 
 public class ScreenBuffer {
 	
-	ArrayList<TileMarker> alMarkers = null;
+	ArrayList<Image> alTiles = null;
+	Image cursor = null;
+	Image mark = null;
+	long tl_x, tl_y, tr_x, tr_y, bl_x, bl_y, br_x, br_y;
+	
+	public ScreenBuffer(String mapfile) {
+		
+		int i;
+		try {
+			Image img0 = new Image("res/tile.png");
+			Image img1 = new Image("res/tile2.png");
+			Image imgx = new Image("res/tile_x.png");
+			Image imgy = new Image("res/tile_y.png");
+			
+			cursor = new Image("res/mouse.png");
+			mark = new Image("res/tile3.png");
+			
+			InputStreamReader in = new InputStreamReader(new FileInputStream(mapfile), "US-ASCII");
+			alTiles = new ArrayList<Image>();
+			while ((i = in.read()) != -1) {
+				switch (i) {
+				case '0': 
+					alTiles.add(img0);
+					break;
+				case '1':
+					alTiles.add(img1);
+					break;
+				case 'x':
+					alTiles.add(imgx);
+					break;
+				case 'y':
+					alTiles.add(imgy);
+					break;
+				}
+			}
+			in.close();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
 
-	public ScreenBuffer() {
-		alMarkers = new ArrayList<TileMarker>();
 	}
 	
-	class TileMarker {
-		int start;
-		int length;
+	public void render(int x_off, int y_off) {
+		int i, j, x, y;
+		// Shifting works because effective tile dimensions are all base 2 values.
+		for (i = 0; i < Const.MAP_SIZE; i++) {
+			x = i << 4;
+			for (j = Const.MAP_SIZE - 1; j >= 0; j--) {
+		    	y = j << 4;
+		        alTiles.get(i * Const.MAP_SIZE + j).draw((x << 1) + (y << 1) + x_off, (x - y) + y_off);
+		    }
+		}
+		cursor.draw(512.0f, 384.0f);
 		
-		TileMarker(int s, int l) {
-			start = s;
-			length = l;
+		if (tl_x > -1 && tl_x < 256 && tl_y > -1 && tl_y < 256) {
+			mark.draw((tl_x << 5) + (tl_y << 5) + x_off, (tl_y << 4) - (tl_x << 4) + y_off);
+		}
+		if (tr_x > -1 && tr_x < 256 && tr_y > -1 && tr_y < 256) {
+			mark.draw((tr_x << 5) + (tr_y << 5) + x_off, (tr_y << 4) - (tr_x << 4) + y_off);
+		}
+		if (bl_x > -1 && bl_x < 256 && bl_y > -1 && bl_y < 256) {
+			mark.draw((bl_x << 5) + (bl_y << 5) + x_off, (bl_y << 4) - (bl_x << 4) + y_off);
+		}
+		if (br_x > -1 && br_x < 256 && br_y > -1 && br_y < 256) {
+			mark.draw((br_x << 5) + (br_y << 5) + x_off, (br_y << 4) - (br_x << 4) + y_off);
 		}
 	}
+	
+	public void update(int x_off, int y_off) {
+		long base_x = Math.round((-x_off + 2*y_off)/64.0);
+		long base_y = Math.round((-x_off - 2*y_off)/64.0);
+		tl_x = base_x;
+		tl_y = base_y - 1;		
+		tr_x = base_x + 16;
+		tr_y = base_y + 15;
+		bl_x = base_x - 24;
+		bl_y = base_y + 23;
+		br_x = base_x - 8;
+		br_y = base_y + 39;
+	}
+	
 }
